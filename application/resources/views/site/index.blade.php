93d8e3362a1a6b0947569e5408c87bcdbb6e2fa4
Daniel Berg <danielbergmorais@gmail.com>
2024-07-09 15:26:49 -0300

    Adicionado paginas do site com a programação funcional

--- application/resources/views/site/index.blade.php
+++ application/resources/views/site/index.blade.php
@@ -5,42 +5,23 @@
 <section class="hero">
     <div class="container">
         <div class="hero__slider owl-carousel">
-            <div class="hero__items set-bg" data-setbg="{{ asset('assets/site/img/tower-of-god.jpg') }}">
+
+            @foreach($bannerPosts as $bannerPost)
+            <div class="hero__items set-bg" data-setbg="{{ asset('storage/blog/'. $bannerPost->file ) }}">
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="hero__text">
-                            <div class="label">Animes</div>
-                            <h2>Data de lançamento da 2ª temporada de Tower of God é revelada</h2>
-                            <p>Prepare-se para voltar à torre</p>
-                            <a href="/noticia"><span>Saiba mais</span> <i class="fa fa-angle-right"></i></a>
+                            <div class="label">{{ $bannerPost->category->name }}</div>
+                            <h2>{{ \Illuminate\Support\Str::limit($bannerPost->title , 100, $end='...')}}</h2>
+                            <p>{{ \Illuminate\Support\Str::limit($bannerPost->subtitle , 50, $end='...')}}</p>
+                            <a href="{{ route('site.post', $bannerPost) }}">
+                                <span>Saiba mais</span> <i class="fa fa-angle-right"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="hero__items set-bg" data-setbg="{{ asset('assets/site/img/venom.webp') }}">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="hero__text">
-                            <div class="label">Filmes</div>
-                            <h2>O que você precisa saber sobre Venom: The Last Dance</h2>
-                            <p>A sequência de Venom chega aos cinemas no dia 28 de outubro deste ano!</p>
-                            <a href="/noticia"><span>Saiba mais</span> <i class="fa fa-angle-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="hero__items set-bg" data-setbg="{{ asset('assets/site/img/wukong.webp') }}">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="hero__text">
-                            <div class="label">Jogos</div>
-                            <h2>Black Myth: Wukong será lançado em 20 de agosto</h2>
-                            <p>Trailer também revela que um dos principais artefatos de Sun Wukong estará no game</p>
-                            <a href="/noticia"><span>Saiba mais</span> <i class="fa fa-angle-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </section>
@@ -61,62 +42,29 @@
                             </div>
                         </div>
                     </div>
+
                     <div class="row">
+                        @foreach($indexPosts as $indexPost)
                         <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="{{ asset('assets/site/img/zzz.webp') }}">
-                                    <div class="ep">Jogos</div>
+                                <div class="product__item__pic set-bg" data-setbg="{{ asset('storage/blog/'. $indexPost->file ) }}">
+                                    <div class="ep">{{ $indexPost->category->name }}</div>
                                 </div>
                                 <div class="product__item__text">
                                     <ul>
-                                        <li>06/07/2024</li>
+                                        <li>{{ date('d/m/Y', strtotime($indexPost->created_at)) }}</li>
                                     </ul>
-                                    <h5><a href="#">Zenless Zone Zero, da HoYoverse, já está disponível</a></h5>
+                                    <h5>
+                                        <a href="{{ route('site.post', $indexPost) }}">
+                                            {{ \Illuminate\Support\Str::limit($indexPost->title , 100, $end='...')}}
+                                        </a>
+                                    </h5>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 col-sm-6">
-                            <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="{{ asset('assets/site/img/elden.webp') }}">
-                                    <div class="ep">Jogos</div>
-                                </div>
-                                <div class="product__item__text">
-                                    <ul>
-                                        <li>06/07/2024</li>
-                                    </ul>
-                                    <h5><a href="#">Novo trailer cinematográfico de Elden Ring: Shadow of the Erdtree apresenta trama de Messmer e Miquella</a></h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6 col-sm-6">
-                            <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="{{ asset('assets/site/img/metaphor.jpg') }}">
-                                    <div class="ep">Jogos</div>
-                                </div>
-                                <div class="product__item__text">
-                                    <ul>
-                                        <li>06/07/2024</li>
-                                    </ul>
-                                    <h5><a href="#">Metaphor: ReFantazio, novo RPG dos criadores de Persona, ganha trailer estonteante e data de lançamento</a></h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6 col-sm-6">
-                            <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="{{ asset('assets/site/img/wukong.webp') }}">
-                                    <div class="ep">06/07/2024</div>
-                                </div>
-                                <div class="product__item__text">
-                                    <ul>
-                                        <li>Jogos</li>
-                                    </ul>
-                                    <h5><a href="#">Metaphor: ReFantazio, novo RPG dos criadores de Persona, ganha trailer estonteante e data de lançamento</a></h5>
-                                </div>
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
-
             </div>
 
             @include('site.parts.sidebar')
--- application/resources/views/site/pages/details.blade.php
+++ application/resources/views/site/pages/details.blade.php
@@ -1,6 +1,5 @@
 @extends('site.layouts.blog')
 
-
 @section('content')
 <!-- Anime Section Begin -->
 <section class="anime-details spad">
@@ -9,28 +8,31 @@
             <div class="row">
                 <div class="col-lg-12 mb-5">
                     <div class="anime__details__title">
-                        <h3>Data de lançamento da 2ª temporada de Tower of God é revelada</h3>
-                        <span>Prepare-se para voltar à torre</span>
+                        <h3>{{ $post->title }}</h3>
+                        <span>{{ $post->subtitle }}</span>
                     </div>
-                    <div class="anime__details__pic set-bg" data-setbg="{{ asset('assets/site/img/tower-of-god.jpg') }}">
-                        <!--<div class="comment"><i class="fa fa-comments"></i> 11</div>
-                        <div class="view"><i class="fa fa-eye"></i> 9141</div>-->
+                    @if($post->file)
+                    <div class="anime__details__pic set-bg" data-setbg="{{ asset('storage/blog/'. $post->file ) }}">
                     </div>
+                    @endif
                 </div>
 
                 <div class="col-lg-8">
                     <div class="anime__details__text ">
-                        <p>Já sabíamos que a segunda temporada de Tower of God chegaria no dia 7 de julho, mas agora temos o horário exato que o tão esperado anime retorna à Crunchyroll. Você poderá assistir à 2ª temporada de Tower of God na Crunchyroll a partir de 7 de julho de 2024, às 11:00 (horário de Brasília).</p>
+                        <p>{{ $post->content }}</p>
+
                         <div class="anime__details__widget">
-                            <iframe width="100%" height="400" src="https://www.youtube.com/embed/wMqlnGZRUZ8" frameborder="0" allow="accelerometer;  encrypted-media; gyroscope;" allowfullscreen></iframe>
+                            @if($post->youtube)
+                            <iframe width="100%" height="400" src="{{ $post->youtube }}" frameborder="0" allow="accelerometer;  encrypted-media; gyroscope;" allowfullscreen></iframe>
+                            @endif
                         </div>
                         <p>
-                            <span>Fonte: Crunchyroll</span>
+                            <span>Publicado em {{ date('d/m/Y', strtotime($post->created_at)) }}</span>
                         </p>
                         <div class="anime__details__btn">
-                            <a href="#">
+                            <a href="{{ route('site.posts', $post->category) }}">
                                 <span class="follow-btn">
-                                    Anime
+                                    {{ $post->category->name }}
                                 </span>
                             </a>
                         </div>
