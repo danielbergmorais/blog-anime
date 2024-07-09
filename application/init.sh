b71e79bd7a09a4e69773453237747b88c5b54767
Daniel Berg <danielbergmorais@gmail.com>
2024-07-09 18:10:00 -0300

    Adicionado imagens para carga inicial de posts

--- application/init.sh
+++ application/init.sh
@@ -5,4 +5,4 @@ php artisan key:generate
 php artisan storage:link
 php artisan migrate
 php artisan db:seed
-mv database/seeders/noticias storage/app/public/
+mv database/seeders/images storage/app/public/blog
