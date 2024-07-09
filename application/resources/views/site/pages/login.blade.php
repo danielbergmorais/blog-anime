35876f5ce4626802a7de4adabef662b025928595
Daniel Berg <danielbergmorais@gmail.com>
2024-07-09 15:03:08 -0300

    Adicionado login via Breeze Ajuste na nomeclatura de arquivos Removido arquivos não utilizados

--- application/resources/views/site/pages/login.blade.php
+++ application/resources/views/site/pages/login.blade.php
@@ -8,15 +8,34 @@
             <div class="col-lg-6">
                 <div class="login__form">
                     <h3>Login</h3>
-                    <form action="#">
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                        {{ $error }}
+                        @endforeach
+                    </div>
+                    @endif
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
                         <div class="input__item">
-                            <input type="text" placeholder="Seu Email ">
+                            <input type="email" placeholder="Seu Email " value="{{ old('email') }}" name="email" required autofocus autocomplete="username">
                             <span class="icon_mail"></span>
+
+                            @error('email')
+                            <span class="invalid-feedback text-danger" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
                         <div class="input__item">
-                            <input type="password" placeholder="Sua Senha">
+                            <input type="password" placeholder="Sua Senha" type="password" name="password" required autocomplete="current-password">
                             <span class="icon_lock"></span>
                         </div>
+                        @error('password')
+                        <span class="invalid-feedback text-danger" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                         <button type="submit" class="site-btn">Entrar</button>
                     </form>
                     <!--<a href="#" class="forget_pass">Esqueceu a senha?</a>-->
