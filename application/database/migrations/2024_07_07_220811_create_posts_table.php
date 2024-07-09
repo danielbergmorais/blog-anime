e66ee73ba97df0c0777b9a78bde24bedaae60b77
Daniel Berg <danielbergmorais@gmail.com>
2024-07-09 14:50:32 -0300

    Ajuste no model de Post Ajuste no migration de User e Posts Adicionado seeder de Posts

---
+++ application/database/migrations/2024_07_07_220811_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('subtitle')->nullable();
+            $table->string('youtube')->nullable();
+            $table->string('file');
+            $table->text('content');
+            $table->tinyInteger('active')->default(1);;
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('RESTRICT');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('RESTRICT');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
